eb5144d023e0a07ddf87f4de08922b1703b17f11
Jamil Khan <44854522+jamilkhan98@users.noreply.github.com>
2019-10-30 17:13:28 -0700

    Add files via upload

--- Code/Frontend/src/App.js
+++ Code/Frontend/src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Background from "./psBackgroundImg.jpg"
 import Navbar from './Navbar';
 import Header from './Header';
 import Footer from './Footer';
 
-function App() {
+function App() {  
+
+  const styles2 = {
+    backgroundSize: "cover",
+    backgroundImage: "url(" +  Background  + ")",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "fixed",
+
+    minHeight: "100%",
+    minWidth: "100%"
+  }
+
   return (
-    <div>
+    <div class = "container-fluid" style = {styles2}>
       <Navbar />
     </div>
   );
--- Code/Frontend/src/Navbar.js
+++ Code/Frontend/src/Navbar.js
@@ -1,27 +1,33 @@
 import React from "react"
-import Background from "./psBackgroundImg.jpg"
+// import Background from "./psBackgroundImg.jpg"
 
 function Navbar(){
 
   const styles = {
+    backgroundColor: "white",
     border: "3px solid green",
     margin: "140px 280px",
     padding: "60px",
     display: "inline-block",
     textAlign: "center",
-    maxWidth: "75%",
-    backgroundImage: "url(" +  Background  + ")"
+    position: "relative",
+    boxSizing: "inherit"
+    // backgroundImage: "url(" +  Background  + ")"
   }
+
+  // const styles2 = {
+  //   backgroundImage: "url(" +  Background  + ")"
+  // }
 
     return(
       <span>
-        <form style = {styles}>
+        <form class = "container-fluid" style = {styles}>
           Username
           <input type="text"></input>
           <br/><br/>
-          Password<br/> 
+          Password 
           <input type="text"></input>
-          <br/><br/>
+          <br/>
           <input type="submit" value="Submit"></input>
         </form>
       </span>
