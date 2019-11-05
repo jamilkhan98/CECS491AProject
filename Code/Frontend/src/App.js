9879723e9329ca63ac1b5196838a673ceef51f0f
Jamil Khan <44854522+jamilkhan98@users.noreply.github.com>
2019-11-04 17:12:06 -0800

    Add files via upload

--- Code/Frontend/src/App.js
+++ Code/Frontend/src/App.js
@@ -7,7 +7,6 @@ import Background from "./psBackgroundImg.jpg"
 import Navbar from './Navbar';
 import Header from './Header';
 import Footer from './Footer';
-import HelloWorldApp from './HelloWorldApp';
 
 function App() {  
 
