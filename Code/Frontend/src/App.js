008accfea2c4eb81c019656f62387135012564db
punch2dface <benseo000@gmail.com>
2019-11-06 17:36:49 -0800

    Add files via upload

--- Code/Frontend/src/App.js
+++ Code/Frontend/src/App.js
@@ -8,6 +8,7 @@ import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
 import { NavigationBar } from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
+import SignIn from './SignIn';
 class App extends Component {
 
   render(){
@@ -23,6 +24,7 @@ class App extends Component {
                 <Route path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
                 <Route path="/createAccount" component={CreateAccount} />
+                <Route path="/signIn" component={SignIn} />
                 <Route component={NoMatch} />
               </Switch>
             </Router>
