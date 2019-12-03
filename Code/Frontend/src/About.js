240d1056206febf78af541fd504fe380666608fd
Chaz Del Prato <chaz.delprato@student.csulb.edu>
2019-12-02 16:56:09 -0800

    Update About.js

--- Code/Frontend/src/About.js
+++ Code/Frontend/src/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const About = () => (
     <div>
         <h2> ABOUT PAGE</h2>
-        <p>Let all these little things happen. Don't fight them. Learn to use them. It's so important to do something every day that will make you happy. These trees are so much fun. I get started on them and I have a hard time stopping. I started painting as a hobby when I was little. I didn't know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do.</p>
-        <p>Anytime you learn something your time and energy are not wasted. Painting should do one thing. It should put happiness in your heart. In your world you can create anything you desire. We'll do another happy little painting.</p>
-        <p>Automatically, all of these beautiful, beautiful things will happen. Everybody's different. Trees are different. Let them all be individuals. There is immense joy in just watching - watching all the little creatures in nature. You don't have to spend all your time thinking about what you're doing, you just let it happen. Let's put a touch more of the magic here. This is a fantastic little painting.</p>
+        <p>We are all just trying to function.</p>
+        <p>We are tyring our best.</p>
+        <p>This is our rough alpha version of our Pick-up sport application.</p>
     </div>
 )
