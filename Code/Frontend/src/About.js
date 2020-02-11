78cb2379fe14c0c400cfb187977a53ad9e43a1e1
Chaz Del Prato <chaz.delprato@student.csulb.edu>
2020-02-10 17:29:13 -0800

    Add files via upload

---
+++ Code/Frontend/src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => (
+    <div>
+        <h2> ABOUT PAGE</h2>
+        <p> A little about us. We are a developing a Computer Science Senior Project that is in relation to finding a pick-up sport easyly</p>
+        <p> We are currently on our second semester and are working diligently working to a completed project.</p>
+    </div>
+)
---
+++ Code/Frontend/src/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contact = () => (
+    <div>
+        <h2> CONTACT PAGE</h2>
+        <p> If any problems, please contact us:  </p>
+        <p> ECS 413, Table 3</p>
+    </div>
+)
