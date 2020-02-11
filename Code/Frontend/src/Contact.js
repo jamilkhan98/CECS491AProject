bfc15d5dacd6bea96faa9c72dd588fa197bbaeb9
Chaz Del Prato <chaz.delprato@student.csulb.edu>
2020-02-10 17:30:29 -0800

    Delete Contact.js

--- Code/Frontend/src/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Contact = () => (
-    <div>
-        <h2> CONTACT PAGE</h2>
-        <p> If any problems, please contact us:  </p>
-        <p> ECS 413, Table 3</p>
-    </div>
-)
